527e5f06fdcb5e79290a49b93c5e45be29fda7f1
RehaanX <96602659+RehaanX@users.noreply.github.com>
2022-05-25 18:42:23 -0700

    Update ComputerSecurity.java

--- ComputerClass/ComputerSecurity.java
+++ ComputerClass/ComputerSecurity.java
@@ -1,3 +1,4 @@
+// This is the child class of computer
 class ComputerSecurity extends Computer{
     String user;
     public ComputerSecurity(String user){
@@ -7,39 +8,4 @@ class ComputerSecurity extends Computer{
       return "The last user to accsess the " + (super.name) + " was " + this.user;
     }
   }
-    /* This is the computer class which is stored in another file:
-class Computer{
-  String name = "";
-  String model; 
-  String lastDate;
-  public Computer() {}
-  
-  
-  public Computer(String name, String model){
-    this.name = name;
-    this.model = model;
-  }
-  public String lastDate(){
-    java.util.Date date = new java.util.Date(); 
-    return date.toString();
-  }
-}
-
-  /*
-  /* This is the main class which is stored in another file:
-import java.time.format.DateTimeFormatter;  
-import java.time.LocalDateTime;   
-import java.util.Scanner;
-class Main{
-  public static void main(String[] args){
-    Scanner input = new Scanner(System.in);
-    Computer laptop = new Computer("laptop", "XI");
-    System.out.println(laptop.lastDate());
-    
-    Computer sec = new ComputerSecurity("Reehan");
-    System.out.println(sec.lastDate());
-  }
-}
-
-  }
-  */ 
+   
